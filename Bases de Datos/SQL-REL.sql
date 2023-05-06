6fb67724a1721e0cf06a7bb870a459f71b277902
ALEX B-DEV <alexbetbu@gmail.com.com>
2023-05-05 20:26:12 -0500

    Cargando bases de datos y graficas

---
+++ Bases de Datos/SQL-REL.sql
@@ -0,0 +1,138 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.11
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: localhost:3306
+-- Tiempo de generación: 05-05-2023 a las 19:20:57
+-- Versión del servidor: 5.7.23-23
+-- Versión de PHP: 7.4.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `softmedi_enyoi-mod2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `mascota`
+--
+
+CREATE TABLE `mascota` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `genero` varchar(255) NOT NULL,
+  `raza` varchar(255) NOT NULL,
+  `propietarioId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `mascota`
+--
+
+INSERT INTO `mascota` (`id`, `nombre`, `genero`, `raza`, `propietarioId`) VALUES
+(1, 'Maga', 'Hembra', 'Perro Criollo', 1017236361),
+(2, 'Michu', 'Macho', 'Gato Bengalí', 54782222),
+(3, 'Luna', 'Hembra', 'Perro Schnauzer', 58874555),
+(4, 'Kenay', 'Macho', 'Perro Cocker Spaniel', 1017161462),
+(5, 'Mia', 'Hembra', 'Perro Schnauzer', 58874555),
+(6, 'Coffie', 'Macho', 'Perro Pekinés', 1017258745),
+(7, 'Mia', 'Hembra', 'Perro Criollo', 1017789658),
+(8, 'Andriod', 'Macho', 'Gato Criollo', 1029161875);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `persona`
+--
+
+CREATE TABLE `persona` (
+  `cedula` int(11) NOT NULL,
+  `nombre` varchar(255) NOT NULL,
+  `apellido` varchar(255) NOT NULL,
+  `edad` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `persona`
+--
+
+INSERT INTO `persona` (`cedula`, `nombre`, `apellido`, `edad`) VALUES
+(54782222, 'Luz Angela', 'Gil', 50),
+(58874555, 'Jean', 'Silva', 35),
+(1017161462, 'Alexander', 'Betancur', 35),
+(1017236361, 'Manuela', 'Betancur', 27),
+(1017258745, 'Daniela', 'Tabares', 29),
+(1017789658, 'Erika', 'Builes', 29),
+(1029161875, 'Eduardo', 'Fonseca', 35);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `veterinaria`
+--
+
+CREATE TABLE `veterinaria` (
+  `nombre` varchar(255) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `telefono` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `veterinaria`
+--
+
+INSERT INTO `veterinaria` (`nombre`, `direccion`, `telefono`) VALUES
+('Veterinaria Patitas Felices', 'Calle 10 # 00-00', '+57-510-000-000');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `propietarioId` (`propietarioId`);
+
+--
+-- Indices de la tabla `persona`
+--
+ALTER TABLE `persona`
+  ADD PRIMARY KEY (`cedula`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `mascota`
+--
+ALTER TABLE `mascota`
+  ADD CONSTRAINT `propietarioId` FOREIGN KEY (`propietarioId`) REFERENCES `persona` (`cedula`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
